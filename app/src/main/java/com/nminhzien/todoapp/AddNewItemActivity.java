69088925c7c1e33697d4c3452387a843912bdf05
Minh Nguyen <minh.nguyen@windriver.com>
2017-06-12 14:04:38 +0700

    Add menu bar

--- app/src/main/java/com/nminhzien/todoapp/AddNewItemActivity.java
+++ app/src/main/java/com/nminhzien/todoapp/AddNewItemActivity.java
@@ -1,38 +1,36 @@
 package com.nminhzien.todoapp;
 
 import android.content.Intent;
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.EditText;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 import static com.nminhzien.todoapp.MainActivity.ITEM_NAME;
 import static com.nminhzien.todoapp.MainActivity.ITEM_POSITION;
+import static com.nminhzien.todoapp.MainActivity.NEW_ITEM;
 
-public class EditItemActivity extends AppCompatActivity {
 
-    String todoItemName;
-    int pos;
-    @BindView(R.id.et_EditItem) EditText et_EditItem;
-    @BindView(R.id.edit_item_toolbar) Toolbar edit_item_toolbar;
+public class AddNewItemActivity extends AppCompatActivity {
+
+    @BindView(R.id.et_new_item) EditText et_new_item;
+    @BindView(R.id.new_item_toolbar) Toolbar new_item_toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_edit_item);
+        setContentView(R.layout.activity_add_new_item);
 
         ButterKnife.bind(this);
-        setSupportActionBar(edit_item_toolbar);
-
-        todoItemName = getIntent().getStringExtra(ITEM_NAME);
-        pos = getIntent().getExtras().getInt(ITEM_POSITION);
-        et_EditItem.setText(todoItemName);
+        setSupportActionBar(new_item_toolbar);
     }
 
     @Override
@@ -47,7 +45,7 @@ public class EditItemActivity extends AppCompatActivity {
         switch (item.getItemId()) {
             case R.id.save_new_item:
                 // User chose the "Settings" item, show the app settings UI...
-                onSaveItem();
+                onSaveNewItem();
                 return true;
 
             case R.id.cancel_action:
@@ -62,11 +60,11 @@ public class EditItemActivity extends AppCompatActivity {
         }
     }
 
-    public void onSaveItem(){
+    public void onSaveNewItem(){
         Intent data = new Intent();
-        data.putExtra(ITEM_NAME, et_EditItem.getText().toString());
-        data.putExtra(ITEM_POSITION, pos);
+        data.putExtra(NEW_ITEM, et_new_item.getText().toString());
         setResult(RESULT_OK, data);
         this.finish();
     }
+
 }
